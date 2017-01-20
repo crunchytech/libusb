40921b142c769b3116f5d3130667832f22b573f7
blakdragan7 <jcopela4@gmail.com>
2017-01-20 16:01:59 -0500

    2015

---
+++ startupscript.bat
@@ -0,0 +1,16 @@
+
+net start wudfsvc
+ping 0.0.0.0
+net start wudfsvc
+ping 0.0.0.0
+net start wudfsvc
+ping 0.0.0.0
+ping 0.0.0.0
+ping 0.0.0.0
+ping 0.0.0.0
+cd C:\Users\Crunchy\Desktop\ObjectTracking
+start ObjectDetection.exe
+ping 0.0.0.0
+ping 0.0.0.0
+start C:\Users\Crunchy\Desktop\Build\WindowsNoEditor\BowlingFrontEnd.exe
+exit
